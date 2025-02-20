00090329fa9c9184c73fa1fd0374b4cccbe38155
Nitin <sharmanitan938@gmail.com>
2025-02-20 09:47:08 +0530

    Adding Map functionality in Project

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,7 @@
 const Listing = require("../models/listing");
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req,res) => {
     const allListings   = await Listing.find({});
@@ -24,6 +27,13 @@ module.exports.showListing = async (req,res) => {
 }
 
 module.exports.createListing = async (req,res, next) => { 
+
+    let response = await geocodingClient.forwardGeocode({
+        query: req.body.listing.location,
+        limit: 1
+      })
+    .send();
+        
     let url= req.file.path;
     let filename = req.file.filename;
     // console.log(url , ".. " , filename ) 
@@ -31,8 +41,12 @@ module.exports.createListing = async (req,res, next) => {
     // console.log(req.user);
     newListing.owner = req.user._id;
     newListing.image = {url , filename};
-    await newListing.save();
+
+    newListing.geometry = response.body.features[0].geometry;
+
+    let saveListing = await newListing.save();
     req.flash("success" , "New Listing Created!");
+    console.log(saveListing);
     // console.log(newListing);
     res.redirect("/listings")
     
