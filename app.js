b63c342318de9560e6b6f1d2bd6af71f509fb8c7
Nitin <sharmanitan938@gmail.com>
2025-02-21 18:28:14 +0530

    Cloud database using Mongodb atlas and adding session using connect-mongo package

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");// express session requiring
+const MongoStore = require('connect-mongo');// for mongo session store
 const flash = require("connect-flash");
 const passport = require("passport");// Authentication and Authorization
 const LocalStrategy = require("passport-local");
@@ -20,9 +21,11 @@ const listingRouter = require("./routes/listing.js") ; // requiring listing rout
 const reviewRouter = require("./routes/review.js")
 const userRouter = require("./routes/user.js");
 
+let dbUrl = process.env.ATLASDB_URL;
+
 main().then(() => {console.log("Connected to DB")}).catch(err => console.log(err));
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+    await mongoose.connect(dbUrl);
 }
 
 app.set("view engine" , "ejs");
@@ -32,7 +35,20 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const store = MongoStore.create({
+    mongoUrl : dbUrl,
+    crypto : {
+        secret : "mysupersecretcode"
+    },
+    touchAfter : 24 * 3600,
+});
+
+store.on("error" , () => {
+    console.log("ERROR in MONGO SESSION STORE" , err);
+})
+
 const sessionOption = {
+     store,
     secret : "mysupersecretcode",
     resave : false,
     saveUninitialized : true,
